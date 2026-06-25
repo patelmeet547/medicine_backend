555dcfe699d52d4e4a9cf798918e513a8568206b
VMeet053 <v.meet0503@gmail.com>
2026-06-25 22:10:57 +0530

    Fix CORS and Vercel routing

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ try {
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
